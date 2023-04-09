760fd5c2c03a445dcc6f725a2f36498f5aaaca1d
Fernando Borretti <fernando@borretti.me>
2023-04-10 09:35:39 +1000

    Style fixes

--- sequence.py
+++ sequence.py
@@ -35,11 +35,13 @@ from dataclasses import dataclass
 
 # Types.
 
+
 @dataclass(frozen=True)
 class Elem:
     """
     An element in the sequence.
     """
+
     # Indices are humanized: the first card has index 1.
     index: int
     text: str
@@ -47,14 +49,18 @@ class Elem:
     def __post_init__(self):
         assert self.index > 0
 
+
 @dataclass(frozen=True)
 class Card:
     """
     A flashcard.
     """
+
     text: str
 
+
 # Main loop.
+
 
 def main():
     # Read all lines from stdin.
@@ -63,8 +69,7 @@ def main():
     title: str = lines[0]
     # The remaining lines are the elements of the sequence.
     elems: list[Elem] = [
-        Elem(index=index+1, text=text)
-        for index, text in enumerate(lines[1:])
+        Elem(index=index + 1, text=text) for index, text in enumerate(lines[1:])
     ]
     # Accumulator for generated cards.
     cards: list[Card] = []
@@ -83,22 +88,35 @@ def main():
     # The predecessor cards ask you to remember what element comes before each.
     cards += make_predecessor(elems)
     # Render cards.
-    writer = csv.writer(sys.stdout, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL, lineterminator="\n")
+    writer = csv.writer(
+        sys.stdout,
+        delimiter=",",
+        quotechar='"',
+        quoting=csv.QUOTE_ALL,
+        lineterminator="\n",
+    )
     for card in cards:
         writer.writerow([render_card(card, title)])
+
 
 def make_test_card(elems: list[Elem]) -> Card:
     lst: str = "\n".join("1. " + elem.text for elem in elems)
     text: str = f"Recall all elements of the sequence.\n---\n{lst}"
     return Card(text=text)
 
+
 def make_cloze_card(elems: list[Elem]) -> Card:
-    lst: str = "\n".join("1. {{" + str(idx+1) + "::" + elem.text + "}}" for idx, elem in enumerate(elems))
+    lst: str = "\n".join(
+        "1. {{" + str(idx + 1) + "::" + elem.text + "}}"
+        for idx, elem in enumerate(elems)
+    )
     text: str = f"Elements of the sequence.\n---\n{lst}"
     return Card(text=text)
 
+
 def render_card(card: Card, title: str) -> str:
     return f"# {title}\n\n{card.text}"
+
 
 def make_forward(elems: list[Elem]) -> list[Card]:
     cards: list[Card] = []
@@ -107,12 +125,14 @@ def make_forward(elems: list[Elem]) -> list[Card]:
         cards.append(Card(text=text))
     return cards
 
+
 def make_backward(elems: list[Elem]) -> list[Card]:
     cards: list[Card] = []
     for elem in elems:
         text: str = f"What is the position of: {elem.text}\n---\n{elem.index}"
         cards.append(Card(text=text))
     return cards
+
 
 def make_successor(elems: list[Elem]) -> list[Card]:
     cards: list[Card] = []
@@ -123,6 +143,7 @@ def make_successor(elems: list[Elem]) -> list[Card]:
             text: str = f"What comes after: {elem.text}\n---\n{succ.text}"
             cards.append(Card(text=text))
     return cards
+
 
 def make_predecessor(elems: list[Elem]) -> list[Card]:
     cards: list[Card] = []
